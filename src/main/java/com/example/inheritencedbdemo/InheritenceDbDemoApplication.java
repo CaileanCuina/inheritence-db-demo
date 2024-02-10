25e8474ad98ab45bc6e7ba8b7de2bf5394a98145
nina.trausner <nina.trausner@gmail.com>
2024-02-10 10:54:34 +0100

    Initial commit

---
+++ src/main/java/com/example/inheritencedbdemo/InheritenceDbDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.inheritencedbdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InheritenceDbDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InheritenceDbDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/inheritencedbdemo/InheritenceDbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inheritencedbdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InheritenceDbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
